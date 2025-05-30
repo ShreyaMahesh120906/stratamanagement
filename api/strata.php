c882fe356180453b009ec2a203aee8d7026b6455
ShreyaMahesh120906 <smah0632@uni.sydney.edu.au>
2025-05-31 09:31:26 +1000

    Create strata.php

---
+++ api/strata.php
@@ -0,0 +1,8 @@
+<?php
+echo "<h1>Strata Management Updates</h1>";
+echo "<ul>";
+echo "<li>Garbage collection every Tuesday</li>";
+echo "<li>Pool maintenance on 5th June</li>";
+echo "<li>Fire drill on 20th June</li>";
+echo "</ul>";
+?>
